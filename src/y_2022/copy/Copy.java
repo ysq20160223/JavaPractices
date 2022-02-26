b2472e5104101b1bfb94360eeae102fe8d2276f4
ysq20160223 <398183931@qq.com>
2022-02-26 14:35:16 +0800

    update copy

--- src/y_2022/copy/Copy.java
+++ src/y_2022/copy/Copy.java
@@ -5,6 +5,7 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.Arrays;
 
 import javax.swing.filechooser.FileSystemView;
 
@@ -17,20 +18,54 @@ public class Copy {
 		File homeFile = FileSystemView.getFileSystemView().getHomeDirectory();
 		XLog.init().debug(homeFile);
 
-		// copyImg(homeFile + "\\Img\\BitCoin.png", homeFile + "\\Img\\BitCoin",
-		// "BitCoin", 5000);
-		copyImg(homeFile + "\\Img\\nzha.png", homeFile + "\\Img\\nzha", "nzha", 5000);
+		// copyImg(new File(homeFile + "\\Img\\BitCoin.png"), 5000);
+		// copyImg(new File(homeFile + "\\Img\\nzha.png"), 5000);
+		copyImg(new File(homeFile + "\\Img\\Colorful.png"), 5000);
+		// copyImg(new File(homeFile + "\\Img\\Dolphin.png"), 5000);
 	}
 
-	/**
-	 * 
-	 */
-	public static void copyImg(String srcPath, String desPath, String name, int count) throws Exception {
-		for (int i = 0; i < count; i++) {
-			String num = String.format("\\%s_%06d.png", name, i);
+	public static void copyImg(File srcFile, int count) throws Exception {
+		// 判断源文件
+		if (!srcFile.exists()) {
+			XLog.init().warn("srcFile is null");
+			return;
+		}
+		XLog.init().warn("srcFile: " + srcFile);
 
-			BufferedInputStream bis = new BufferedInputStream(new FileInputStream(srcPath));
-			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(desPath + num));
+		// 判断源文件名
+		String srcFileName = srcFile.getName();
+		if (null == srcFileName || srcFileName.length() <= 0) {
+			XLog.init().warn("srcFileName is null");
+			return;
+		}
+		XLog.init().warn("srcFileName: " + srcFileName);
+
+		// 分割源文件名
+		String[] srcFileNameSplit = srcFileName.split("\\.");
+		XLog.init().warn(Arrays.deepToString(srcFileNameSplit));
+		if (null == srcFileNameSplit || srcFileNameSplit.length < 1) {
+			XLog.init().warn("srcFileName is error");
+			return;
+		}
+
+		String prefix = srcFileNameSplit[0]; // 目标文件名
+		String suffix = srcFileNameSplit[1]; // 目标文件后缀名 suffix
+
+		File destDir = new File(srcFile.getParentFile() + "\\" + prefix);
+		if (!destDir.exists()) {
+			destDir.mkdirs();
+		}
+		XLog.init().debug("destDir: " + destDir);
+
+		for (int i = 0; i < count; i++) {
+			String destFileName = String.format("%s_%06d.%s", prefix, i, suffix);
+			File destFile = new File(destDir + "\\" + destFileName);
+
+			FileInputStream in = new FileInputStream(srcFile);
+			FileOutputStream out = new FileOutputStream(destFile);
+
+			BufferedInputStream bis = new BufferedInputStream(in);
+			BufferedOutputStream bos = new BufferedOutputStream(out);
 
 			for (int len = 0; (len = bis.read()) != -1;) {
 				bos.write(len);
@@ -38,7 +73,7 @@ public class Copy {
 
 			bos.close();
 			bis.close();
-			XLog.init().debug("" + num);
+			XLog.init().debug("" + destFileName);
 		}
 	}
 
